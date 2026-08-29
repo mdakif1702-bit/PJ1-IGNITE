474d5799be20b27d57d737ec69a8c29390074d0c
mdakif1702-bit <mdakif1702@gmail.com>
2026-08-29 17:18:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -31,26 +31,12 @@ const albums = {
     class: {
 
         title: "CLASS MOMENTS",
-
-        photos: [
-            "images/class/class01.jpeg"
-        ]
-
     },
 
 
     sports: {
 
         title: "SPORTS & ACTIVITIES",
-
-        photos: [
-            "https://images.unsplash.com/photo-1461896836934-ffe607ba8211",
-            "https://images.unsplash.com/photo-1517649763962-0c623066013b",
-            "https://images.unsplash.com/photo-1538805060514-97d9cc17730c",
-            "https://images.unsplash.com/photo-1552674605-db6ffd4facb5",
-            "https://images.unsplash.com/photo-1518611012118-696072aa579a",
-            "https://images.unsplash.com/photo-1579952363873-27f3bade9f55"
-        ]
 
     },
 
@@ -59,30 +45,12 @@ const albums = {
 
         title: "ACHIEVEMENTS",
 
-        photos: [
-            "https://images.unsplash.com/photo-1560089000-7433a4ebbd64",
-            "https://images.unsplash.com/photo-1530549387789-4c1017266635",
-            "https://images.unsplash.com/photo-1461896836934-ffe607ba8211",
-            "https://images.unsplash.com/photo-1579952363873-27f3bade9f55",
-            "https://images.unsplash.com/photo-1552674605-db6ffd4facb5",
-            "https://images.unsplash.com/photo-1517649763962-0c623066013b"
-        ]
-
     },
 
 
     random: {
 
         title: "RANDOM MOMENTS",
-
-        photos: [
-            "https://images.unsplash.com/photo-1527529482837-4698179dc6ce",
-            "https://images.unsplash.com/photo-1529156069898-49953e39b3ac",
-            "https://images.unsplash.com/photo-1511632765486-a01980e01a18",
-            "https://images.unsplash.com/photo-1529333166437-7750a6dd5a70",
-            "https://images.unsplash.com/photo-1543269865-cbf427effbad",
-            "https://images.unsplash.com/photo-1517457373958-b7bdd4587205"
-        ]
 
     }
 
